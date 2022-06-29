3d359509ff4d8a456ee20e84b640bd62c55ac883
Yiming Chen <yiming.chen699@gmail.com>
2022-06-29 17:05:18 -0400

    fixed path issue and added imagehash dependency to setup

--- examples/user_script.py
+++ examples/user_script.py
@@ -2,14 +2,13 @@ import time, os, sys
 from image_data_quality.image_dataset import ImageDataset
 
 start = time.time()
-imgset = ImageDataset()
+imgset = ImageDataset("./examples/")
 
 print("The path is:", imgset.path) 
 print("There are ", str(len(imgset.image_files)), "images in the dataset")
-print(imgset.audit_images(False, 3)) #returns the dictionary of issues and pandas dataframe of information
+print(imgset.audit_images(True, 3)) #returns the dictionary of issues and pandas dataframe of information
 print(imgset.issue_info)
 
 end = time.time()
 total_time = end - start
 print("\n"+ str(total_time))
-
