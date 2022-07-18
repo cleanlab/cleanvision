b071cb42aae17bf45dd7a928f33829a3be5603db
Yiming Chen <yiming.chen699@gmail.com>
2022-07-18 00:14:42 -0400

    Restructured near dup check, bug fixes, added blurry check, static check, took care of nested lists, added kwargs

--- examples/user_script.py
+++ examples/user_script.py
@@ -1,13 +1,14 @@
 import time, os, sys
+import imagehash
 from image_data_quality.image_dataset import ImageDataset
 
 start = time.time()
 imgset = ImageDataset("./examples/")
-
+kwargs = {"Near Duplicates" : {"hashtype": imagehash.whash}}
 print("The path is:", imgset.path) 
 print("There are ", str(len(imgset.image_files)), "images in the dataset")
-print(imgset.audit_images(True, 3)) #returns the dictionary of issues and pandas dataframe of information
-print(imgset.issue_info)
+issues = imgset.find_issues(verbose=False, num_preview=15, threshold = 5, issues_checked=None, **kwargs) #the dictionary of issues and pandas dataframe of information
+print(issues)
 
 end = time.time()
 total_time = end - start
