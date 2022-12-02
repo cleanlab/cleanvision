aad731ad858ed0bfd88dbe440fc9dd796252c46f
Jonas Mueller <1390638+jwmueller@users.noreply.github.com>
2022-12-02 13:52:03 -0800

    add TODO

--- examples/user_script.py
+++ examples/user_script.py
@@ -6,6 +6,7 @@ images = ImageDataset("image_files/", thumbnail_size = (128,128))
 print("The path is:", images.path)
 print("There are ", str(len(images.image_files)), "images in the dataset")
 issues = images.find_issues(threshold = None, issues_checked={"Near Duplicates":{"hash_size":8}}, verbose=False, num_preview=0) #the dictionary of issues and pandas dataframe of information
+# TODO: add code here showing how to view just the images with a particular type of issue once `issues` has been computed
 #print(images.misc_info['Near Duplicate Image Groups'])
 print(issues)
 #issues checked make into a new dictionaries, values are keyword arguments (hyperparameter)
