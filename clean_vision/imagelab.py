9acca4ff6f31eeb7e2d0201e1ab62e00a8ae82a2
Sanjana <sanjanag@users.noreply.github.com>
2023-01-04 03:05:02 +0530

    Added check for white images (#34)
    * Changes
    - Added visualization manager for different kinds of visualization
    - Added check for light images that reuses compute
    - Updated demo notebook
    
    * Added different examples in run.py on how to use imagelab'
    
    * Modified code to run imagelab.find_issues() incrementally
    
    * Resolved PR comment
    
    * Changed image_path column to an index for issues dataframe
    
    * Changed ImageProperty to be a constant string
    
    * Updated comment
    
    * change skip_set to defer_set
    
    * Small refactoring
    
    * Changed hyperparameter to dictionary
    
    * Bug fixes
    
    * Added _set_default_config method
    
    * Moved non user facing files to utils
    
    * Added issue_name variable to issue_manager
    
    * Changed verbosity to an int argument which sets the default args for report
    
    * Changed issue manager to a python property
    
    * fix: 🩹 remove classmethod from issue_name property
    
    Chaining @classmethod and @property results is deprecated (removed in python 3.11).
    Setting a class variable for concrete classes should work just fine.
    
    * style: 🎨 invert threshold condition
    
    The main logic of the `set_hyperparameters` method is no longer nested.
    
    * style: 🎨 rename parrameter in `update_summary`
    
    The `summary` attribute is a dataframe, while the parameter (with the same name) is a dictionary. Renaming the parameter should avoid any confusion.
    
    * refactor: 🔥 remove unnessary dataframe initialization
    
    the `issues` dataframe is already created when instantiating the issue manager.
    
    * style: ⚰️ remove commented line in __init__
    
    * refactor: ♻️ remove unused variables/parameters
    
    * refactor: ♻️ iterrows -> itertuples
    
    This allows us to remove the unused index in the iterator.
    
    * style: 🎨 apply black formatter
    
    * style: 🚨 drop f-string formatting for string without a placeholder
    
    * Added back dataframe initialization
    
    Co-authored-by: Elías Snorrason <eliassno@gmail.com>

--- clean_vision/imagelab.py
+++ clean_vision/imagelab.py
@@ -1,45 +1,68 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-from PIL import Image
+import math
 
+import pandas as pd
+
+from clean_vision.utils.constants import IMAGE_PROPERTY
 from clean_vision.utils.issue_manager_factory import _IssueManagerFactory
-from clean_vision.issue_types import IssueType
+from clean_vision.utils.issue_types import IssueType
 from clean_vision.utils.utils import get_filepaths
+from clean_vision.utils.viz_manager import VizManager
 
 
 class Imagelab:
     def __init__(self, data_path):
         self.filepaths = get_filepaths(data_path)
+        self.num_images = len(self.filepaths)
         self.info = {}
         self.issue_summary = pd.DataFrame()
-        self.issues = pd.DataFrame(self.filepaths, columns=["image_path"])
-        self.issue_types = list(IssueType)
-        self.issue_managers = []
+        self.issues = pd.DataFrame(index=self.filepaths)
+        self.issue_types = []
+        self.issue_managers = {}
+        # can be loaded from a file later
+        self.config = self._set_default_config()
+
+    def _set_default_config(self):
+        return {
+            "visualize_num_images_per_row": 4,
+            "report_num_top_issues_values": [3, 5, 10, len(self.issue_types)],
+            "report_examples_per_issue_values": [4, 8, 16, 32],
+            "report_max_prevalence": 0.5,
+        }
+
+    def _get_issues_to_compute(self, issue_types):
+        if issue_types is None or len(issue_types) == 0:
+            all_issues = list(IssueType)
+        else:
+            all_issues = []
+            for issue_type_str, hyperparameters in issue_types.items():
+                issue_type = IssueType(issue_type_str)
+                issue_type.set_hyperparameters(hyperparameters)
+                all_issues.append(issue_type)
+        to_compute_issues = list(set(all_issues) - set(self.issue_types))
+        return to_compute_issues
 
     def find_issues(self, issue_types=None):
-        if issue_types is not None and len(issue_types) > 0:
-            self.issue_types = []
-            for issue_type_str, threshold in issue_types.items():
-                issue_type = IssueType(issue_type_str)
-                issue_type.threshold = threshold
-                self.issue_types.append(issue_type)
-
+        to_compute_issues = self._get_issues_to_compute(issue_types)
+        self.issue_types.extend(to_compute_issues)
         print(
-            f"Checking for {', '.join([issue_type.value for issue_type in self.issue_types])} images"
+            f"Checking for {', '.join([issue_type.value for issue_type in to_compute_issues])} images ..."
         )
 
         # create issue managers
         self._set_issue_managers()
 
-        for issue_manager in self.issue_managers:
+        for issue_manager in self.issue_managers.values():
             issue_manager.find_issues(self.filepaths, self.info)
 
             # update issues, issue_summary and info
-            self.issues = self.issues.merge(
-                issue_manager.issues, how="left", on="image_path"
+            self.issues = self.issues.join(issue_manager.issues, how="left")
+            self.issue_summary = pd.concat(
+                [self.issue_summary, issue_manager.summary], axis=0, ignore_index=True
             )
-            self.issue_summary = pd.concat([self.issue_summary, issue_manager.summary])
             self.info = {**self.info, **issue_manager.info}
+        self.issue_summary = self.issue_summary.sort_values(
+            by=["num_images"], ascending=False
+        )
 
         return
 
@@ -49,60 +72,91 @@ class Imagelab:
             if issue_type.property:
                 image_property_issues.append(issue_type)
             else:
-                self.issue_managers.append(
-                    _IssueManagerFactory.from_str(issue_type.value)
+                self.issue_managers[issue_type] = _IssueManagerFactory.from_str(
+                    issue_type.value
                 )
-        if len(image_property_issues) > 0:
-            self.issue_managers.append(
-                _IssueManagerFactory.from_str("ImageProperty")(image_property_issues)
+
+        if image_property_issues:
+            if IMAGE_PROPERTY in self.issue_managers:
+                self.issue_managers[IMAGE_PROPERTY].add_issue_types(
+                    image_property_issues
+                )
+            else:
+                self.issue_managers[IMAGE_PROPERTY] = _IssueManagerFactory.from_str(
+                    IMAGE_PROPERTY
+                )(image_property_issues)
+
+    def _get_topk_issues(self, num_top_issues, max_prevalence):
+        topk_issues = []
+        for row in self.issue_summary.itertuples(index=False):
+            if getattr(row, "num_images") / self.num_images < max_prevalence:
+                topk_issues.append(getattr(row, "issue_type"))
+        return topk_issues[:num_top_issues]
+
+    def _get_report_args(self, verbosity, user_supplied_args):
+        report_args = {
+            "num_top_issues": self.config["report_num_top_issues_values"][
+                verbosity - 1
+            ],
+            "max_prevalence": self.config["report_max_prevalence"],
+            "examples_per_issue": self.config["report_examples_per_issue_values"][
+                verbosity - 1
+            ],
+        }
+
+        non_none_args = {
+            arg: value for arg, value in user_supplied_args.items() if value is not None
+        }
+
+        return {**report_args, **non_none_args}
+
+    def report(
+        self,
+        num_top_issues=None,
+        max_prevalence=None,
+        examples_per_issue=None,
+        verbosity=1,
+    ):
+        assert isinstance(verbosity, int) and 0 < verbosity < 5
+        user_supplied_args = locals()
+        report_args = self._get_report_args(verbosity, user_supplied_args)
+
+        top_issues = self._get_topk_issues(
+            report_args["num_top_issues"], report_args["max_prevalence"]
+        )
+        top_issue_summary = self.issue_summary[
+            self.issue_summary["issue_type"].isin(top_issues)
+        ]
+
+        print("Top issues in the dataset\n")
+        print(top_issue_summary.to_markdown(), "\n")
+
+        self.visualize(top_issues, report_args["examples_per_issue"])
+
+    def _visualize(self, issue_type_str, examples_per_issue, figsize):
+        if issue_type_str in [
+            IssueType.DARK_IMAGES.value,
+            IssueType.LIGHT_IMAGES.value,
+        ]:
+            sorted_df = self.issues.sort_values(by=[f"{issue_type_str}_score"])
+            sorted_df = sorted_df[sorted_df[f"{issue_type_str}_bool"] == 1]
+            if len(sorted_df) < examples_per_issue:
+                print(
+                    f"Found only {len(sorted_df)} examples of {issue_type_str} issue in the dataset."
+                )
+            else:
+                print(f"\nTop {examples_per_issue} images with {issue_type_str} issue")
+            sorted_filepaths = sorted_df.index[:examples_per_issue].tolist()
+            VizManager.property_based(
+                filepaths=sorted_filepaths,
+                nrows=math.ceil(
+                    min(examples_per_issue, len(sorted_filepaths))
+                    / self.config["visualize_num_images_per_row"]
+                ),
+                ncols=self.config["visualize_num_images_per_row"],
+                figsize=figsize,
             )
 
-    def report(self, verbose=False):
-        topk = 5
-        sorted_df = self.issue_summary.sort_values(by=["num_images"])
-        if verbose:
-            print("Issues in the dataset sorted by prevalence")
-            print(sorted_df.to_markdown())
-        else:
-
-            print(f"Top {topk} issues in the dataset\n")
-            print(sorted_df.head(topk).to_markdown())
-        self._visualize()
-
-    def _visualize(self, topk=5):
-        sorted_df = self.issue_summary.sort_values(by=["num_images"])
-        topk_issues = sorted_df["issue_type"].tolist()[:topk]
-        image_paths = []
-        num_images_per_issue = 4
-        for issue_type in topk_issues:
-            sorted_df = self.issues.sort_values(by=[f"{issue_type}_score"])
-            sorted_filepaths = (
-                sorted_df["image_path"].head(num_images_per_issue).tolist()
-            )
-            image_paths.extend(sorted_filepaths)
-
-        nrows, ncols = (
-            int(len(image_paths) / num_images_per_issue),
-            num_images_per_issue,
-        )
-
-        fig, axes2d = plt.subplots(
-            nrows=max(nrows, 2), ncols=ncols, sharex=True, sharey=True, figsize=(10, 10)
-        )
-
-        img_idx = 0
-        for i, row in enumerate(axes2d):
-            if i == nrows:
-                break
-            for j, ax in enumerate(row):
-                ax.imshow(Image.open(image_paths[img_idx]))
-                ax.set_xticklabels([])
-                ax.set_xticks([])
-                ax.set_yticklabels([])
-                ax.set_yticks([])
-                ax.set_title(image_paths[img_idx].split("/")[-1], fontsize=5)
-                if j == 0:
-                    ax.set_ylabel(f"{topk_issues[i]}", rotation=0, labelpad=20)
-                img_idx += 1
-
-        plt.show()
+    def visualize(self, issue_types, examples_per_issue=4, figsize=(8, 8)):
+        for issue_type in issue_types:
+            self._visualize(issue_type, examples_per_issue, figsize)
