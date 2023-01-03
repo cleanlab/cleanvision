e385e1f0b3dcf758dbdfef3634d5be18be4904c3
Sanjana Garg <sanjana@cleanlab.ai>
2023-01-04 01:17:07 +0530

    Added back dataframe initialization

--- clean_vision/issue_managers/image_property_issue_manager.py
+++ clean_vision/issue_managers/image_property_issue_manager.py
@@ -69,6 +69,7 @@ class ImagePropertyIssueManager(IssueManager):
                         )
 
         # Init issues, summary, info
+        self.issues = pd.DataFrame(index=filepaths)
         summary_dict = {}
         self.info = {}
 
