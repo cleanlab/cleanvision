953cb8f340397b42d5249d42d79059e5d9fe29c7
Sanjana Garg <sanjana@cleanlab.ai>
2023-01-07 13:13:21 +0530

    Fixed bug in imagelab._update_issues

--- cleanvision/imagelab.py
+++ cleanvision/imagelab.py
@@ -98,14 +98,15 @@ class Imagelab:
         )
 
     def _update_issues(self, issue_manager_issues):
-        columns_to_update = list(
-            set(issue_manager_issues.columns).intersection(set(self.issues.columns))
-        )
-        if len(columns_to_update) > 0:
-            for column_name in columns_to_update:
-                self.issues[column_name] = issue_manager_issues[column_name]
-        else:
-            self.issues = self.issues.join(issue_manager_issues, how="left")
+        columns_to_update, new_columns = [], []
+        for column in issue_manager_issues.columns:
+            if column in self.issues.columns:
+                columns_to_update.append(column)
+            else:
+                new_columns.append(column)
+        for column_name in columns_to_update:
+            self.issues[column_name] = issue_manager_issues[column_name]
+        self.issues = self.issues.join(issue_manager_issues[new_columns], how="left")
 
     def _get_image_property_issues(self, issue_types_with_params):
         image_property_issues = {}
@@ -133,79 +134,88 @@ class Imagelab:
         for row in self.issue_summary.itertuples(index=False):
             if getattr(row, "num_images") / self.num_images < max_prevalence:
                 topk_issues.append(getattr(row, "issue_type"))
-        return topk_issues[:num_top_issues]
-
-    def _get_report_args(self, verbosity, user_supplied_args):
-        report_args = {
-            "num_top_issues": self.config["report_num_top_issues_values"][
-                verbosity - 1
-            ],
-            "max_prevalence": self.config["report_max_prevalence"],
-            "examples_per_issue": self.config["report_examples_per_issue_values"][
-                verbosity - 1
-            ],
-        }
-
-        non_none_args = {
-            arg: value for arg, value in user_supplied_args.items() if value is not None
-        }
-
-        return {**report_args, **non_none_args}
-
-    def report(
-        self,
-        num_top_issues=None,
-        max_prevalence=None,
-        examples_per_issue=None,
-        verbosity=1,
-    ):
-        assert isinstance(verbosity, int) and 0 < verbosity < 5
-        user_supplied_args = locals()
-        report_args = self._get_report_args(verbosity, user_supplied_args)
-
-        top_issues = self._get_topk_issues(
-            report_args["num_top_issues"], report_args["max_prevalence"]
-        )
-        top_issue_summary = self.issue_summary[
-            self.issue_summary["issue_type"].isin(top_issues)
-        ]
-
-        print("Top issues in the dataset\n")
-        print(top_issue_summary.to_markdown(), "\n")
-
-        self.visualize(top_issues, report_args["examples_per_issue"])
-
-    def _get_viz_name(self, issue_type_str):
-        if issue_type_str in IMAGE_PROPERTY_ISSUE_TYPES_LIST:
-            viz_name = self.issue_managers[IMAGE_PROPERTY].visualization
-        else:
-            viz_name = self.issue_managers[issue_type_str].visualization
-        return viz_name
-
-    def _visualize(self, issue_type_str, examples_per_issue, figsize):
-        viz_name = self._get_viz_name(issue_type_str)
-        viz_method = VIZ_REGISTRY[viz_name]
-
-        if viz_name == "property_based":
-            sorted_df = self.issues.sort_values(by=[f"{issue_type_str}_score"])
-            sorted_df = sorted_df[sorted_df[f"{issue_type_str}_bool"] == 1]
-            if len(sorted_df) < examples_per_issue:
-                print(
-                    f"Found only {len(sorted_df)} examples of {issue_type_str} issue in the dataset."
-                )
             else:
-                print(f"\nTop {examples_per_issue} images with {issue_type_str} issue")
-            sorted_filepaths = sorted_df.index[:examples_per_issue].tolist()
-            viz_method(
-                filepaths=sorted_filepaths,
-                nrows=math.ceil(
-                    min(examples_per_issue, len(sorted_filepaths))
-                    / self.config["visualize_num_images_per_row"]
-                ),
-                ncols=self.config["visualize_num_images_per_row"],
-                figsize=figsize,
-            )
+                print(
+                    f"Removing {getattr(row, 'issue_type')} from potential issues in the dataset as it exceeds "
+                    f"max_prevalence={max_prevalence} "
+                )
+                return topk_issues[:num_top_issues]
 
-    def visualize(self, issue_types, examples_per_issue=4, figsize=(8, 8)):
-        for issue_type in issue_types:
-            self._visualize(issue_type, examples_per_issue, figsize)
+        def _get_report_args(self, verbosity, user_supplied_args):
+            report_args = {
+                "num_top_issues": self.config["report_num_top_issues_values"][
+                    verbosity - 1
+                ],
+                "max_prevalence": self.config["report_max_prevalence"],
+                "examples_per_issue": self.config["report_examples_per_issue_values"][
+                    verbosity - 1
+                ],
+            }
+
+            non_none_args = {
+                arg: value
+                for arg, value in user_supplied_args.items()
+                if value is not None
+            }
+
+            return {**report_args, **non_none_args}
+
+        def report(
+            self,
+            num_top_issues=None,
+            max_prevalence=None,
+            examples_per_issue=None,
+            verbosity=1,
+        ):
+            assert isinstance(verbosity, int) and 0 < verbosity < 5
+            user_supplied_args = locals()
+            report_args = self._get_report_args(verbosity, user_supplied_args)
+
+            top_issues = self._get_topk_issues(
+                report_args["num_top_issues"], report_args["max_prevalence"]
+            )
+            top_issue_summary = self.issue_summary[
+                self.issue_summary["issue_type"].isin(top_issues)
+            ]
+
+            print("Top issues in the dataset\n")
+            print(top_issue_summary.to_markdown(), "\n")
+
+            self.visualize(top_issues, report_args["examples_per_issue"])
+
+        def _get_viz_name(self, issue_type_str):
+            if issue_type_str in IMAGE_PROPERTY_ISSUE_TYPES_LIST:
+                viz_name = self.issue_managers[IMAGE_PROPERTY].visualization
+            else:
+                viz_name = self.issue_managers[issue_type_str].visualization
+            return viz_name
+
+        def _visualize(self, issue_type_str, examples_per_issue, figsize):
+            viz_name = self._get_viz_name(issue_type_str)
+            viz_method = VIZ_REGISTRY[viz_name]
+
+            if viz_name == "property_based":
+                sorted_df = self.issues.sort_values(by=[f"{issue_type_str}_score"])
+                sorted_df = sorted_df[sorted_df[f"{issue_type_str}_bool"] == 1]
+                if len(sorted_df) < examples_per_issue:
+                    print(
+                        f"Found only {len(sorted_df)} examples of {issue_type_str} issue in the dataset."
+                    )
+                else:
+                    print(
+                        f"\nTop {examples_per_issue} images with {issue_type_str} issue"
+                    )
+                sorted_filepaths = sorted_df.index[:examples_per_issue].tolist()
+                viz_method(
+                    filepaths=sorted_filepaths,
+                    nrows=math.ceil(
+                        min(examples_per_issue, len(sorted_filepaths))
+                        / self.config["visualize_num_images_per_row"]
+                    ),
+                    ncols=self.config["visualize_num_images_per_row"],
+                    figsize=figsize,
+                )
+
+        def visualize(self, issue_types, examples_per_issue=4, figsize=(8, 8)):
+            for issue_type in issue_types:
+                self._visualize(issue_type, examples_per_issue, figsize)
--- examples/run.py
+++ examples/run.py
@@ -15,13 +15,13 @@ if __name__ == "__main__":
     imagelab.find_issues(issue_types)
     imagelab.report()
 
-    # Check for additional types of issues using existing Imagelab
-    issue_types = {"Light": {}}
+    # Check for an issue with a different threshold
+    issue_types = {"Dark": {"threshold": 0.2}}
     imagelab.find_issues(issue_types)
     imagelab.report()
 
-    # Check for an issue with a different threshold
-    issue_types = {"Dark": {"threshold": 0.2}}
+    # Check for additional types of issues using existing Imagelab
+    issue_types = {"Light": {}}
     imagelab.find_issues(issue_types)
     imagelab.report()
 
