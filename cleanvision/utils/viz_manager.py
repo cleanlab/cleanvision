731f25d3fa458915284d36ac25bebe4d5cf9b2aa
Ulyana <ulyana@cleanlab.ai>
2023-01-19 15:11:06 -0800

    Fix typing for viz_manager.py

--- cleanvision/utils/viz_manager.py
+++ cleanvision/utils/viz_manager.py
@@ -1,25 +1,34 @@
 import math
+from typing import TypeVar, List, Tuple, Set
 
 import matplotlib.pyplot as plt  # type: ignore
 from PIL import Image  # type: ignore
 from mpl_toolkits.axes_grid1 import ImageGrid  # type: ignore
 
+TVizManager = TypeVar("TVizManager", bound="VizManager")  # self type for the class
+
 
 class VizManager:
-    def __init__(self):
+    def __init__(self: TVizManager):
         pass
 
     @staticmethod
-    def individual_images(filepaths, ncols, cell_size):
+    def individual_images(
+        filepaths: List[str], ncols: int, cell_size: Tuple[int, int]
+    ) -> None:
         plot_image_grid(filepaths, ncols, cell_size)
 
     @staticmethod
-    def image_sets(filepath_sets, ncols, cell_size):
+    def image_sets(
+        filepath_sets: Set[List[str]], ncols: int, cell_size: Tuple[int, int]
+    ) -> None:
         for s in filepath_sets:
             plot_image_grid(s, ncols, cell_size)
 
 
-def plot_image_grid(filepaths, ncols, cell_size):
+def plot_image_grid(
+    filepaths: List[str], ncols: int, cell_size: Tuple[int, int]
+) -> None:
     nrows = math.ceil(len(filepaths) / ncols)
     ncols = min(ncols, len(filepaths))
     fig = plt.figure(figsize=(cell_size[0] * ncols, cell_size[1] * nrows))
