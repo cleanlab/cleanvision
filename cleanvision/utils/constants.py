8bb570713a2efd683f261d9cd1ff31e5e51585ef
Ulyana <ulyana@cleanlab.ai>
2023-01-19 14:50:55 -0800

    Fixed typing in constants.py

--- cleanvision/utils/constants.py
+++ cleanvision/utils/constants.py
@@ -1,10 +1,12 @@
+from typing import List
+
 IMAGE_PROPERTY: str = "image_property"
 DUPLICATE: str = "duplicate"
-IMAGE_PROPERTY_ISSUE_TYPES_LIST = [
+IMAGE_PROPERTY_ISSUE_TYPES_LIST: List[str] = [
     "dark",
     "light",
     "odd_aspect_ratio",
     "low_information",
 ]
-DUPLICATE_ISSUE_TYPES_LIST = ["exact_duplicates", "near_duplicates"]
+DUPLICATE_ISSUE_TYPES_LIST: List[str] = ["exact_duplicates", "near_duplicates"]
 SETS: str = "sets"
