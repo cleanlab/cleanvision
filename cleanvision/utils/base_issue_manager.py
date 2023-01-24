888cee4e0d8cf21eefd51f5fdcd1b8bac4d2302a
Ulyana <ulyana@cleanlab.ai>
2023-01-23 19:37:32 -0800

    Addressed comments with base issue manager

--- cleanvision/utils/base_issue_manager.py
+++ cleanvision/utils/base_issue_manager.py
@@ -11,11 +11,11 @@ TIssueManager = TypeVar(
 class IssueManager(ABC):
     """Base class for managing data issues of a particular type in Imagelab."""
 
-    def __init__(self: TIssueManager, params: Dict[Any, Any]):
-        self.info: Dict[str, Dict[Any, Any]] = {"statistics": {}}
+    def __init__(self: TIssueManager, params: Dict[str, Any]):
+        self.info: Dict[str, Dict[str, Any]] = {"statistics": {}}
         self.issues: pd.DataFrame = pd.DataFrame()
         self.summary: pd.DataFrame = pd.DataFrame(columns=["issue_type"])
-        self.params: Dict[Any, Any] = self.get_default_params()
+        self.params: Dict[str, Any] = self.get_default_params()
         self.set_params(params)
 
     def __repr__(self: TIssueManager) -> str:
@@ -27,20 +27,20 @@ class IssueManager(ABC):
         return class_name
 
     @abstractmethod
-    def find_issues(self: TIssueManager, *args: List[str], **kwargs: List[str]) -> None:
+    def find_issues(self: TIssueManager, *args: Any, **kwargs: Any) -> None:
         """Finds occurrences of this particular issue in the dataset."""
         raise NotImplementedError
 
     @abstractmethod
-    def get_default_params(self: TIssueManager) -> Dict[Any, Any]:
+    def get_default_params(self: TIssueManager) -> Dict[str, Any]:
         """Returns default params to be used by the issue_manager"""
         raise NotImplementedError
 
     @abstractmethod
-    def set_params(self, params: Dict[Any, Any]) -> None:
+    def set_params(self, params: Dict[str, Any]) -> None:
         """Sets params for an issue manager. Default params will be overridden by user provided params"""
         raise NotImplementedError
 
     @staticmethod
-    def _compute_summary(issues_boolean: "pd.Series[int]") -> Dict[str, int]:
+    def _compute_summary(issues_boolean: "pd.Series[bool]") -> Dict[str, int]:
         return {"num_images": issues_boolean.sum()}
