89b27293e1a1d2ab68a1546062735a740174be53
Sanjana Garg <sanjana@cleanlab.ai>
2023-01-31 11:21:59 -0800

    black formatting

--- examples/run.py
+++ examples/run.py
@@ -14,7 +14,9 @@ if __name__ == "__main__":
     issue_types = {"near_duplicates": {}}
     imagelab.find_issues(issue_types)
     imagelab.report(issue_types.keys())
-    imagelab.save("./results")  # optional, just included to show how to save/load this as a file
+    imagelab.save(
+        "./results"
+    )  # optional, just included to show how to save/load this as a file
 
     # Check for additional types of issues using existing Imagelab
     imagelab = Imagelab.load("./results", dataset_path)
