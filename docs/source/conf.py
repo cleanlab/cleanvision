90499d533064666f665dc21222f5ce6c51571597
Ulyana <ulyana@cleanlab.ai>
2023-02-06 14:46:10 -0800

    Save

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,35 +1,32 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+import datetime
 
-# -- Project information
+sys.path.insert(0, os.path.abspath("../../"))
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Cleanvision'
-copyright = '2023, Cleanlab Inc.'
+project = 'cleanvision'
+copyright = f"{datetime.datetime.now().year}, Cleanlab Inc."
 author = 'Cleanlab'
+release = '2023'
 
-release = '0.1'
-version = '1.0.0'
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# -- General configuration
-
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-]
-
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
+extensions = []
 
 templates_path = ['_templates']
+exclude_patterns = []
 
-# -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
 
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
