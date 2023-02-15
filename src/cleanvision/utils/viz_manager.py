9e76b746c641a0966372d51ce45d3ab50ee3681e
Sanjana Garg <garg.sanjana.95@gmail.com>
2023-02-15 14:46:53 -0800

    Replaced ImageGrid with subplots as ImageGrid was cropping images weirdly, which skipped the main object in the image sometimes

--- src/cleanvision/utils/viz_manager.py
+++ src/cleanvision/utils/viz_manager.py
@@ -25,6 +25,15 @@ class VizManager:
             plot_image_grid(s, ncols, cell_size)
 
 
+def set_image_on_axes(path, ax):
+    image = Image.open(path)
+    cmap = "gray" if image.mode == "L" else None
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    ax.set_title(path.split("/")[-1], fontsize=7)
+    ax.imshow(image, cmap=cmap)
+
+
 def plot_image_grid(
     filepaths: List[str],
     ncols: int,
@@ -32,20 +41,21 @@ def plot_image_grid(
     cmap: Optional[Union[str, Colormap]] = None,
 ) -> None:
     nrows = math.ceil(len(filepaths) / ncols)
-    ncols = min(ncols, len(filepaths))
-    fig = plt.figure(figsize=(cell_size[0] * ncols, cell_size[1] * nrows))
-
-    grid = ImageGrid(fig, 111, nrows_ncols=(nrows, ncols), axes_pad=0.2, share_all=True)
-
-    for ax, path in zip(grid, filepaths):
-        image = Image.open(path)
-        if image.mode == "L" and cmap is None:
-            cmap = "gray"
-        # Iterating over the grid returns the Axes.
-        ax.set_xticklabels([])
-        ax.set_xticks([])
-        ax.set_yticklabels([])
-        ax.set_yticks([])
-        ax.set_title(path.split("/")[-1], fontsize=5)
-        ax.imshow(Image.open(path), cmap=cmap)
+    fig, axes = plt.subplots(
+        nrows, ncols, figsize=(cell_size[0] * ncols, cell_size[1] * nrows)
+    )
+    if nrows > 1:
+        idx = 0
+        for i in range(nrows):
+            for j in range(ncols):
+                idx = i * ncols + j
+                if idx >= len(filepaths):
+                    break
+                set_image_on_axes(filepaths[idx], axes[i, j])
+            if idx >= len(filepaths):
+                break
+    else:
+        for i in range(ncols):
+            if i < len(filepaths):
+                set_image_on_axes(filepaths[i], axes[i])
     plt.show()
