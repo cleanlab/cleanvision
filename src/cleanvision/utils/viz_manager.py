b59d940f761262f008b03ba4534069fa9d4766e1
Sanjana Garg <garg.sanjana.95@gmail.com>
2023-02-15 16:04:23 -0800

    Removed unused parameter cmap and handled showing a grid of just 1 image

--- src/cleanvision/utils/viz_manager.py
+++ src/cleanvision/utils/viz_manager.py
@@ -1,31 +1,28 @@
 import math
-from typing import List, Optional, Tuple, Set, Union
+from typing import List, Tuple, Set
 
+import matplotlib.axes
 import matplotlib.pyplot as plt
 from PIL import Image
-from matplotlib.colors import Colormap
-from mpl_toolkits.axes_grid1 import ImageGrid  # type: ignore
 
 
 class VizManager:
     @staticmethod
     def individual_images(
-        filepaths: List[str],
-        ncols: int,
-        cell_size: Tuple[int, int],
-        cmap: Optional[Union[str, Colormap]] = None,
+        filepaths: List[str], ncols: int, cell_size: Tuple[int, int]
     ) -> None:
-        plot_image_grid(filepaths, ncols, cell_size, cmap)
+        plot_image_grid(filepaths, ncols, cell_size)
 
     @staticmethod
     def image_sets(
         filepath_sets: Set[List[str]], ncols: int, cell_size: Tuple[int, int]
     ) -> None:
-        for s in filepath_sets:
+        for i, s in enumerate(filepath_sets):
+            print(f"Set: {i}")
             plot_image_grid(s, ncols, cell_size)
 
 
-def set_image_on_axes(path, ax):
+def set_image_on_axes(path: str, ax: matplotlib.axes.Axes) -> None:
     image = Image.open(path)
     cmap = "gray" if image.mode == "L" else None
     ax.get_xaxis().set_visible(False)
@@ -35,12 +32,10 @@ def set_image_on_axes(path, ax):
 
 
 def plot_image_grid(
-    filepaths: List[str],
-    ncols: int,
-    cell_size: Tuple[int, int],
-    cmap: Optional[Union[str, Colormap]] = None,
+    filepaths: List[str], ncols: int, cell_size: Tuple[int, int]
 ) -> None:
     nrows = math.ceil(len(filepaths) / ncols)
+    ncols = min(ncols, len(filepaths))
     fig, axes = plt.subplots(
         nrows, ncols, figsize=(cell_size[0] * ncols, cell_size[1] * nrows)
     )
@@ -54,8 +49,10 @@ def plot_image_grid(
                 set_image_on_axes(filepaths[idx], axes[i, j])
             if idx >= len(filepaths):
                 break
-    else:
+    elif ncols > 1:
         for i in range(ncols):
             if i < len(filepaths):
                 set_image_on_axes(filepaths[i], axes[i])
+    else:
+        set_image_on_axes(filepaths[0], axes)
     plt.show()
