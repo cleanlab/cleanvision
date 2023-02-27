c8eb5dc8a6a66a623b84e02abaa3313271b7c03d
Ulyana <ulyana@cleanlab.ai>
2023-02-27 15:32:35 -0800

    Suppress plt.show() output for tests

--- tests/test_run.py
+++ tests/test_run.py
@@ -7,37 +7,15 @@ import numpy as np
 from cleanvision.imagelab import Imagelab
 from examples.custom_issue_manager import CustomIssueManager
 
-# main_script_dir = os.path.dirname(__file__)
-# rel_path = "resources/images/phone.png"
-# PHONE_IMAGE_PATH = os.path.join(main_script_dir, rel_path)
-#
-# dataset_path = "../"
-
-# def test_something_else(tmp_path):
-#     #create a file "myfile" in "mydir" in temp directory
-#     f1 = tmp_path / "mydir/myfile"
-#     f1.parent.mkdir() #create a directory "mydir" in temp folder (which is the parent directory of "myfile"
-#     f1.touch() #create a file "myfile" in "mydir"
-#
-#
-#     #write to file as normal
-#     f1.write_text("text to myfile")
-#
-#     assert f1.read_text() == "text to myfile"
-
-# content of test_tmp_path.py
-CONTENT = "content"
+# to suppress plt.show()
+from unittest.mock import patch
+import matplotlib.pyplot as plt
 
 
 def generate_image(arr=None):
     if arr is None:
-        arr = np.random.randint(
-            low=0,
-            high=256,
-            size=(300, 300, 3),
-            dtype=np.uint8
-        )
-    img = Image.fromarray(arr, mode='RGB')
+        arr = np.random.randint(low=0, high=256, size=(300, 300, 3), dtype=np.uint8)
+    img = Image.fromarray(arr, mode="RGB")
     return img
 
 
@@ -64,20 +42,28 @@ def generate_n_image_files(tmpdir_factory, n=40):
     return str(tmp_image_dir)
 
 
-# contents of test_image.py
-def test_histogram(generate_single_image_file):
+# contents of a single image
+def test_single_image(generate_single_image_file):
     img = Image.open(generate_single_image_file)
-    # compute and test histogram
 
 
-@pytest.mark.usefixtures('generate_n_image_files')
-def test_example1(capsys, generate_n_image_files):
+@pytest.mark.usefixtures("generate_n_image_files")
+def test_example1(capsys, monkeypatch, generate_n_image_files):
+    monkeypatch.setattr(plt, "show", lambda: None)
     imagelab = Imagelab(data_path=generate_n_image_files)  # initialize imagelab
     imagelab.list_default_issue_types()  # list default checks
 
     ## ==== Test list_default_issue_types() lists all default isssue types====
-    DEFAULT_ISSUE_TYPES = ['dark', 'light', 'odd_aspect_ratio', 'low_information', 'exact_duplicates',
-                           'near_duplicates', 'blurry', 'grayscale']
+    DEFAULT_ISSUE_TYPES = [
+        "dark",
+        "light",
+        "odd_aspect_ratio",
+        "low_information",
+        "exact_duplicates",
+        "near_duplicates",
+        "blurry",
+        "grayscale",
+    ]
     captured = capsys.readouterr()
 
     for issue_type in DEFAULT_ISSUE_TYPES:
@@ -112,17 +98,23 @@ def test_example1(capsys, generate_n_image_files):
     # imagelab.visualize(image_files=blurry_images)  # visualize the given image files
 
     ## === Test miscellaneous extra information about datasset
-    assert (list(imagelab.info.keys()) == ['statistics', 'exact_duplicates', 'near_duplicates'])
-    for key in ['color_space', 'entropy', 'brightness', 'blurriness', 'aspect_ratio']:
+    assert list(imagelab.info.keys()) == [
+        "statistics",
+        "exact_duplicates",
+        "near_duplicates",
+    ]
+    for key in ["color_space", "entropy", "brightness", "blurriness", "aspect_ratio"]:
         assert key in list(imagelab.info["statistics"].keys())
 
-@pytest.mark.usefixtures('generate_n_image_files')
-def test_example2(capsys, generate_n_image_files):
+
+@pytest.mark.usefixtures("generate_n_image_files")
+def test_example2(monkeypatch, generate_n_image_files):
+    monkeypatch.setattr(plt, "show", lambda: None)
     imagelab = Imagelab(data_path=generate_n_image_files)  # initialize imagelab
     issue_types = {"near_duplicates": {}}
     imagelab.find_issues(issue_types)
     imagelab.report()
-    save_folder = generate_n_image_files + '/T_save_folder/'
+    save_folder = generate_n_image_files + "/T_save_folder/"
 
     imagelab.save(
         save_folder
@@ -139,8 +131,10 @@ def test_example2(capsys, generate_n_image_files):
     imagelab.find_issues(issue_types)
     imagelab.report(issue_types=issue_types.keys())  # report only specific issues
 
-@pytest.mark.usefixtures('generate_n_image_files')
-def test_example3(capsys, generate_n_image_files):
+
+@pytest.mark.usefixtures("generate_n_image_files")
+def test_example3(monkeypatch, generate_n_image_files):
+    monkeypatch.setattr(plt, "show", lambda: None)
     imagelab = Imagelab(data_path=generate_n_image_files)
     imagelab.find_issues()
     imagelab.report(["near_duplicates"])
@@ -162,8 +156,9 @@ def test_example3(capsys, generate_n_image_files):
     imagelab.visualize(issue_types=["light"], num_images=8, cell_size=(3, 3))
 
 
-@pytest.mark.usefixtures('generate_n_image_files')
-def test_example4(capsys, generate_n_image_files):
+@pytest.mark.usefixtures("generate_n_image_files")
+def test_example4(monkeypatch, generate_n_image_files):
+    monkeypatch.setattr(plt, "show", lambda: None)
     imagelab = Imagelab(data_path=generate_n_image_files)
     issue_name = CustomIssueManager.issue_name
     imagelab.list_possible_issue_types()
