b1457b332d7e04a1207cd857d9f76e64199dd233
Sanjana Garg <garg.sanjana.95@gmail.com>
2023-03-07 12:13:45 -0800

    Black formatting

--- src/cleanvision/utils/utils.py
+++ src/cleanvision/utils/utils.py
@@ -108,9 +108,9 @@ def get_score_colname(issue_type: str) -> str:
 def update_df(
     df: pd.DataFrame, new_df: pd.DataFrame, overwrite: bool = True
 ) -> pd.DataFrame:
-    """ Updates `df` with columns of `new_df` (must have same number of rows).
-          New columns in `new_df` not present in `df` are appended as extra columns.
-          Columns in `df` that are present in `new_df` have their values overwritten if ``overwrite==True``.
+    """Updates `df` with columns of `new_df` (must have same number of rows).
+    New columns in `new_df` not present in `df` are appended as extra columns.
+    Columns in `df` that are present in `new_df` have their values overwritten if ``overwrite==True``.
     """
     columns_to_update, new_columns = [], []
     for column in new_df.columns:
