2e5195b11b301f7995cbd272d96da9feafbc376d
Sanjana Garg <garg.sanjana.95@gmail.com>
2023-03-30 15:28:52 -0700

    Added a variable for num_names

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,10 +7,11 @@ class TestUtils:
     def test_get_filepaths(self, tmp_path):
         img = Image.new("L", (100, 100))
         extensions = [".png", ".jpeg", ".gif", ".jpg"]
-        for i in range(2):
+        num_names = 2
+        for i in range(num_names):
             for ext in extensions:
                 p = tmp_path / f"img_{i}{ext}"
                 img.save(p)
         filepaths = get_filepaths(tmp_path)
         print(filepaths)
-        assert len(filepaths) == len(extensions) * 2
+        assert len(filepaths) == len(extensions) * num_names
