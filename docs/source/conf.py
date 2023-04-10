a76d4f40e413518366b0821299e4a0662a89b2bc
Sanjana Garg <garg.sanjana.95@gmail.com>
2023-04-10 11:21:05 -0500

    Removed unnecessary line

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,8 +34,6 @@ extensions = [
 
 templates_path = ["_templates"]
 
-exclude_patterns = ["cleanvision/issue_managers/*", "cleanvision/utils/*"]
-
 exclude_patterns = []
 
 # --------- Autodoc configuration --------------------------------------------
