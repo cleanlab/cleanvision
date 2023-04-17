ee1dee3badf872e31358ec9f710b2ab6e23a717c
Jonas Mueller <1390638+jwmueller@users.noreply.github.com>
2023-04-16 19:59:28 -0700

    ensure import works on python 3.7

--- src/cleanvision/__init__.py
+++ src/cleanvision/__init__.py
@@ -1,3 +1,5 @@
-import importlib.metadata
-
-__version__ = importlib.metadata.version("cleanvision")
+try:  # not supported on Python 3.7
+    import importlib.metadata
+    __version__ = importlib.metadata.version("cleanvision")
+except Exception:
+    pass
