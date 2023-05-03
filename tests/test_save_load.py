5ea54aec06b819c85a8cbcdce6b23d241e7e0e92
Sanjana Garg <garg.sanjana.95@gmail.com>
2023-05-02 17:52:49 -0700

    Added force=True/False test

--- tests/test_save_load.py
+++ tests/test_save_load.py
@@ -14,9 +14,20 @@ class TestImagelabSaveLoad:
         assert os.path.exists(save_folder / "issues.csv")
         assert os.path.exists(save_folder / "issue_summary.csv")
 
-    def test_force_save(self, generate_local_dataset, tmp_path):
+    @pytest.mark.parametrize(
+        "force",
+        [True, False],
+        ids=["overwrite", "do not overwrite"],
+    )
+    def test_force_save(self, generate_local_dataset, tmp_path, force):
         save_folder = tmp_path / "T_save_folder/"
         save_folder.mkdir()
         imagelab = Imagelab(data_path=generate_local_dataset)
-        with pytest.raises(FileExistsError):
-            imagelab.save(save_folder)
+        if force:
+            imagelab.save(save_folder, force=force)
+            assert os.path.exists(save_folder / "imagelab.pkl")
+            assert os.path.exists(save_folder / "issues.csv")
+            assert os.path.exists(save_folder / "issue_summary.csv")
+        else:
+            with pytest.raises(FileExistsError):
+                imagelab.save(save_folder, force=force)
