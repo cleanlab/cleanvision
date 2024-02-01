8c73b2efddb8bc133b4ebc8358d5f7493239ab3c
Sanjana Garg <garg.sanjana.95@gmail.com>
2024-01-31 23:06:57 -0800

    Fixed typing syntax

--- src/cleanvision/__init__.py
+++ src/cleanvision/__init__.py
@@ -1,12 +1,12 @@
 import sys
-from typing import Any
+from typing import Any, Union
 
 from cleanvision.imagelab import Imagelab as _Imagelab
 
 PYTHON_VERSION_INFO = sys.version_info
 
 
-def get_version() -> str | Any:
+def get_version() -> Union[str, Any]:
     if sys.version_info.major >= 3 and sys.version_info.minor >= 8:
         import importlib.metadata
 
